46224a0e0d7149eed008ac49e20860d510361edf
Andrii Konovalov <andy.smokk@gmail.com>
2024-02-23 15:43:58 +0100

    add threadcard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import { currentUser } from "@clerk/nextjs";
 
 import { fetchPosts } from "@/lib/actions/thread.actions";
+import TreadCard from "@/components/cards/TreadCard";
 
 export default async function Home() {
   const result = await fetchPosts(1, 30);
@@ -10,7 +11,27 @@ export default async function Home() {
     <>
       <h1 className="head-text text-left">Home</h1>
 
-      <section className="mt-9 flex flex-col gap-10"></section>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <TreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
