4c1088790add18776c422862746978d3630b6955
Andrii Konovalov <andy.smokk@gmail.com>
2024-03-01 14:25:17 +0100

    templates for the pages activity, search and community

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,9 @@
+const Page = async () => {
+  return (
+    <section>
+      <h1 className="mt-10 head-text">Activities</h1>
+    </section>
+  );
+};
+
+export default Page;
---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = async () => {
+  return (
+    <section>
+      <h1 className="mt-10 head-text">Search</h1>
+    </section>
+  );
+};
+
+export default Page;
