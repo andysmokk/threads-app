26dfbfe4104d80ae5fff2c5001679132f33c240a
Andrii Konovalov <andy.smokk@gmail.com>
2024-03-23 15:34:53 +0100

    add suggested users

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -47,6 +47,25 @@ const RightSidebar = async () => {
 
       <div className="flex flex-1 flex-col">
         <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+
+        <div className="mt-7 flex w-[350px] flex-col gap-10">
+          {similarMinds.users.length > 0 ? (
+            <>
+              {similarMinds.users.map((person) => (
+                <UserCard
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType="User"
+                />
+              ))}
+            </>
+          ) : (
+            <p className="!text-base-regular text-light-3">No users yet</p>
+          )}
+        </div>
       </div>
     </section>
   );
